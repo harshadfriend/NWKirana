1a17bcb77c9e77c42b099dd4485bbd4957b0d73c
harshadfriend <harshad.friend@gmail.com>
2019-01-05 20:51:25 +0530

    nwkirana

--- app/src/main/java/karjatonline/nw/transactions.java
+++ app/src/main/java/karjatonline/nw/transactions.java
@@ -2,12 +2,20 @@ package karjatonline.nw;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class transactions extends AppCompatActivity {
+    String name,custkey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transactions);
+
+        Bundle extras=getIntent().getExtras();
+        name=extras.getString("name");
+        custkey=extras.getString("custkey");
+
+        Toast.makeText(this, ""+name+"\n"+custkey, Toast.LENGTH_SHORT).show();
     }
 }
