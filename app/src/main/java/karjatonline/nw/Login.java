7591a604008521ef4a8ec28f4b75d5467eabdcf7
harshadfriend <harshad.friend@gmail.com>
2019-01-08 16:34:16 +0530

    commit

--- app/src/main/java/karjatonline/nw/Login.java
+++ app/src/main/java/karjatonline/nw/Login.java
@@ -1,15 +1,25 @@
 package karjatonline.nw;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String pin = "pinKey";
 
+    SharedPreferences sharedpreferences;
+    SharedPreferences.Editor ed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        sharedpreferences = getSharedPreferences(Prefs.StoreKey, Context.MODE_PRIVATE);
+        Toast.makeText(this, ""+sharedpreferences.getString(), Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/karjatonline/nw/Prefs.java
@@ -0,0 +1,6 @@
+package karjatonline.nw;
+
+public class Prefs {
+    public static String StoreKey="Store";
+    public static String PinKey="No";
+}
